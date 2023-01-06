539bf345f0077daa8b48681987ea62f9e85f01ed
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-06 13:05:37 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 console.log("hat JS html link verknüpfung geklappt -> Browser + F12")
+
+let a = 1+3
+console.log(a) // 4
+
+function into() {
+    console.log("Hello World")
+    // console.log("1+3 = " + a)
+    return a + "1+3 =";
+}
+
+into()
